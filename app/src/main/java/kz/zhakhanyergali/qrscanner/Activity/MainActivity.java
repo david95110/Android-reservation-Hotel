fceb344d5e437cab6732ed0ecc0b9260c55ce328
Unknown <Kyrigami@gmail.com>
2019-07-10 15:02:54 +0200

    version stable

--- app/src/main/java/kz/zhakhanyergali/qrscanner/Activity/MainActivity.java
+++ app/src/main/java/kz/zhakhanyergali/qrscanner/Activity/MainActivity.java
@@ -13,6 +13,7 @@ import android.net.Uri;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
--- app/src/main/java/kz/zhakhanyergali/qrscanner/Adapter/HistoryAdapter.java
+++ app/src/main/java/kz/zhakhanyergali/qrscanner/Adapter/HistoryAdapter.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.widget.RecyclerView;
+import android.util.Patterns;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
